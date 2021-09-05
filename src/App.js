cb5c4e9056fe8ff1ac93c31b2f2e54110a9ac043
Abdul Rehman <ars@araxissoft.com>
2021-09-05 17:05:47 +0500

    social

--- src/App.js
+++ src/App.js
@@ -1,56 +1,54 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import $ from 'jquery';
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Services from './Components/Services';
-import Contact from './Components/Contact';
-import Portfolio from './Components/Portfolio';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      foo: 'bar',
-      resumeData: {}
+      foo: "bar",
+      resumeData: {},
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize("UA-110570651-1");
     ReactGA.pageview(window.location.pathname);
-
   }
 
-  getResumeData(){
+  getResumeData() {
     $.ajax({
-      url:'./resumeData.json',
-      dataType:'json',
+      url: "./resumeData.json",
+      dataType: "json",
       cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
+      success: function (data) {
+        this.setState({ resumeData: data });
       }.bind(this),
-      error: function(xhr, status, err){
+      error: function (xhr, status, err) {
         console.log(err);
         alert(err);
-      }
+      },
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getResumeData();
   }
 
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main}/>
-        <Services data={this.state.resumeData.resume}/>
-        <About data={this.state.resumeData.main}/>
+        <Header data={this.state.resumeData.main} />
+        <Services data={this.state.resumeData.resume} />
+        <About data={this.state.resumeData.main} />
         {/* <Portfolio data={this.state.resumeData.Team}/> */}
-        <Contact data={this.state.resumeData.main}/>
-        <Footer data={this.state.resumeData.main}/>
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} />
       </div>
     );
   }
@@ -63,3 +61,13 @@ export default App;
 //   "name": "Abdul Rehman",
 //   "job": "Software Engineer"
 // },
+// {
+//   "name": "twitter",
+//   "url": "http://twitter.com",
+//   "className": "fa fa-twitter"
+// },
+//{
+//   "name": "github",
+//   "url": "",
+//   "className": "fa fa-github"
+// }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,106 +1,149 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
     return (
       <section id="contact">
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
 
-         <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">{message}</p>
+          </div>
+        </div>
 
-            <div className="two columns header-col">
+        <div className="row">
+          <div className="eight columns">
+            <form action="" method="post" id="contactForm" name="contactForm">
+              <fieldset>
+                <div>
+                  <label htmlFor="contactName">
+                    Name <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactName"
+                    name="contactName"
+                    onChange={this.handleChange}
+                  />
+                </div>
 
-               <h1><span>Get In Touch.</span></h1>
+                <div>
+                  <label htmlFor="contactEmail">
+                    Email <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactEmail"
+                    name="contactEmail"
+                    onChange={this.handleChange}
+                  />
+                </div>
 
+                <div>
+                  <label htmlFor="contactSubject">Subject</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactSubject"
+                    name="contactSubject"
+                    onChange={this.handleChange}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactMessage">
+                    Message <span className="required">*</span>
+                  </label>
+                  <textarea
+                    cols="50"
+                    rows="15"
+                    id="contactMessage"
+                    name="contactMessage"
+                  ></textarea>
+                </div>
+
+                <div>
+                  <button className="submit">Submit</button>
+                  <span id="image-loader">
+                    <img alt="" src="images/loader.gif" />
+                  </span>
+                </div>
+              </fieldset>
+            </form>
+
+            <div id="message-warning"> Error boy</div>
+            <div id="message-success">
+              <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
+            </div>
+          </div>
+
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Get In Touch</h4>
+              <p className="address">
+                {/* {name}
+                <br /> */}
+
+                <span>
+                  <a
+                    href="https://wa.me/message/DMGOJKK3EHSBD1"
+                    target="_blank"
+                  >
+                    {" "}
+                    {phone}
+                  </a>
+                </span>
+              </p>
             </div>
 
-            <div className="ten columns">
-
-                  <p className="lead">{message}</p>
-
+            <div className="widget widget_tweets">
+              <a href="https://blog.dreamservices.pk/" target="_blank">
+                {" "}
+                <h4 className="widget-title">Our Blog</h4>
+              </a>
+              <ul id="twitter">
+                <li>
+                  <span>
+                    In light of the high unemployment rates and financial
+                    challenges, it is now our primary duty to guide the educated
+                    youth in the province in the right direction.Youth Affairs,
+                    Sports, Archaeology and Tourism.
+                    <br /> <h4>To know more visit </h4>
+                    <a href="https://blog.dreamservices.pk/" target="_blank">
+                      https://blog.dreamservices.pk
+                    </a>
+                  </span>
+                  <b>
+                    <a href="#">2 Days Ago</a>
+                  </b>
+                </li>
+              </ul>
             </div>
-
-         </div>
-
-         <div className="row">
-            <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					  <h4>Get In Touch</h4>
-					   <p className="address">
-						   {name}<br />
-						   
-						   <span><a href='https://wa.me/message/DMGOJKK3EHSBD1' target="_blank"> {phone}</a></span>
-					   </p>
-				   </div>
-
-               <div className="widget widget_tweets">
-                 <a href='https://blog.dreamservices.pk/' target="_blank"> <h4 className="widget-title">Our Blog</h4></a> 
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        In light of the high unemployment rates and financial challenges, it is now our  primary duty to guide the educated youth in the province in the right direction.Youth Affairs, Sports, Archaeology and Tourism. 
-                        <br/> <h4>To know more visit </h4><a href='https://blog.dreamservices.pk/' target="_blank">https://blog.dreamservices.pk</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside>
-      </div>
-   </section>
+          </aside>
+        </div>
+      </section>
     );
   }
 }
