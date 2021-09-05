1b7e4e24d45da34959d7a4f135d8552e356d8188
Abdul Rehman <ars@araxissoft.com>
2021-09-05 16:14:11 +0500

    fixed card

--- src/Components/ServiceCard.js
+++ src/Components/ServiceCard.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 const WhiteTypography = withStyles({
   root: {
-    color: "#FFFFFF"
-  }
+    color: "#FFFFFF",
+  },
 })(Typography);
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 400,
   },
   media: {
     height: 300,
   },
- 
 });
 
 export default function MediaCard(props) {
@@ -33,26 +32,36 @@ export default function MediaCard(props) {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image={"images/services/"+props.img}
+          image={"images/services/" + props.img}
           title={props.title}
         />
-        <CardContent style={{height:'170px'}}>
-          <Typography gutterBottom variant="h3" component="h2" style={{textAlign:'center'}}>
-          {props.title}
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h3"
+            component="h2"
+            style={{ textAlign: "center" }}
+          >
+            {props.title}
           </Typography>
           <Typography variant="h5" color="textSecondary" component="h4">
-          {props.subtitle}
+            {props.subtitle}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions style={{justifyContent:'center'}} className='services'>
-      <a  href={'https://wa.me/message/DMGOJKK3EHSBD1?text=Hi,%20I%20am%20interested%20in%20'+props.title} target="_blank">
-        <Button size="large" color="primary" fullWidth >
-          
-        <WhiteTypography variant="h5" color="textPrimary" component="h4">
-          Get In Touch
-          </WhiteTypography>
-        </Button>
+      <CardActions style={{ justifyContent: "center" }} className="services">
+        <a
+          href={
+            "https://wa.me/message/DMGOJKK3EHSBD1?text=Hi,%20I%20am%20interested%20in%20" +
+            props.title
+          }
+          target="_blank"
+        >
+          <Button size="large" color="primary" fullWidth>
+            <WhiteTypography variant="h5" color="textPrimary" component="h4">
+              Get In Touch
+            </WhiteTypography>
+          </Button>
         </a>
       </CardActions>
     </Card>
